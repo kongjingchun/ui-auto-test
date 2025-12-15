042b2279eb37548cf890479b6f881546c1b6fe9d
kongjingchun <jing20130808@vip.qq.com>
2025-12-15 15:48:44 +0800

    1、左侧菜单栏定位

---
+++ base/LeftMenuBase.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+# @File  : LeftMenuBase.py
+# @Author: kongjingchun
+# @Date  : 2025/12/15/15:36
+# @Desc  :
+
+class LeftMenuBase:
+    def level_one_menu(self, menu_name):
+        """
+        一级菜单栏
+        :param menu_name:菜单栏名称
+        :return:
+        """
+        return "//aside[@class='el-aside']//span[text()='" + menu_name + "']/parent::li"
+
+
+
+    def level_two_menu(self, menu_name):
+        """
+        二级菜单栏
+        :param menu_name:菜单栏名称
+        :return:
+        """
+        return "//aside[@class='el-aside']//span[text()='" + menu_name + "']/parent::li"
+
+
+if __name__ == '__main__':
+    print(LeftMenuBase().level_one_menu("已卖出的宝贝"))
+
+
