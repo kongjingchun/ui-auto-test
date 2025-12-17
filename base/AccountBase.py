72281dd99070c393b6460bb18925bc4612d91de5
kongjingchun <jing20130808@vip.qq.com>
2025-12-17 17:31:54 +0800

    1、个人资料上传头像

---
+++ base/AccountBase.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+# @File  : AccountBase.py
+# @Author: kongjingchun
+# @Date  : 2025/12/17/17:10
+# @Desc  :
+
+class AccountBase:
+    def basic_info_avatar_input(self):
+        return "//div[@class='avatar-uploader']//input"
+
+    def basic_info_save_button(self):
+        return "//span[text()='保存']/parent::button"
