6a703bd5cf0c597a3cad75dfee1167142b848702
jing2 <jing20130808@vip.qq.com>
2026-01-07 22:11:43 +0800

    添加教师工作台-我教的课相关文件

---
+++ base/teacher_workbench/__init__.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+# @File  : __init__.py
+# @Author: 
+# @Date  : 
+# @Desc  : 教师工作台基础类模块
+
---
+++ page/teacher_workbench/__init__.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+# @File  : __init__.py
+# @Author: 
+# @Date  : 
+# @Desc  : 教师工作台页面对象模块
+
