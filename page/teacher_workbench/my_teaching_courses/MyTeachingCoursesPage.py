b0c8f4b857f3677736b4f7fe6179b2921fa5728b
jing2 <jing20130808@vip.qq.com>
2026-01-07 22:36:47 +0800

    我教的课页面

---
+++ page/teacher_workbench/my_teaching_courses/MyTeachingCoursesPage.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+# @File  : MyTeachingCoursesPage.py
+# @Author:
+# @Date  :
+# @Desc  : 我教的课页面对象类，封装我教的课相关的页面操作方法
+
+from time import sleep
+
+from selenium.webdriver.common.by import By
+
+from base.ObjectMap import ObjectMap
+from base.teacher_workbench.my_teaching_courses.MyTeachingCoursesBase import MyTeachingCoursesBase
+from logs.log import log
+
+
+class MyTeachingCoursesPage(MyTeachingCoursesBase, ObjectMap):
+    """我教的课页面类
+
+    继承MyTeachingCoursesBase和ObjectMap类，提供我教的课页面的元素操作方法
+    """
---
+++ page/teacher_workbench/my_teaching_courses/__init__.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+# @File  : __init__.py
+# @Author: 
+# @Date  : 
+# @Desc  : 我教的课页面对象模块
+
